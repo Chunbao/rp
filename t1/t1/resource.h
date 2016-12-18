05f740f8f686e1a8134d4520a3f4827626073c47
Chunbao <Chunbao@users.noreply.github.com>
2016-12-18 08:02:08 -0600

    Added strategy dialog

--- t1/t1/resource.h
+++ t1/t1/resource.h
@@ -11,29 +11,57 @@
 #define IDI_ICON_HAPPYCAR               130
 #define IDI_ICON_LAMBORGHINI            131
 #define IDI_ICON_PANDA                  132
+#define IDD_DIALOG_Strategy             133
 #define IDC_EXPLORER1                   1000
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON_Refresh              1001
 #define IDC_EDIT1                       1002
 #define IDC_EDIT2                       1003
+#define IDC_EDIT_SECOND_SECOND          1004
+#define IDC_EDIT_SECOND_ADD             1005
 #define IDC_SPIN1                       1006
 #define IDC_COMBO1                      1007
 #define IDC_COMBO2                      1008
+#define IDC_SPIN2                       1008
 #define IDC_CHECK1                      1009
 #define IDC_CHECK_CAPTCHA_ENLARGE       1009
+#define IDC_SPIN3                       1009
 #define IDC_COMBO4                      1010
+#define IDC_SPIN4                       1010
 #define IDC_COMBO3                      1011
+#define IDC_EDIT_FORCE_SEND_MINUTE      1011
 #define IDC_STATIC_PIC                  1012
+#define IDC_EDIT6                       1012
+#define IDC_EDIT_FORCE_SEND_SECOND      1012
 #define IDC_CHECK_CAPTCHA_PREVIEW       1013
+#define IDC_SPIN5                       1013
 #define IDC_COMBO_SEND                  1014
+#define IDC_COMBO_STRATEGY              1015
+#define IDC_EDIT__SECOND_SEND_TIME      1017
+#define IDC_SPIN7                       1018
+#define IDC_SPIN8                       1019
+#define IDC_EDIT_THIRD_MINUTE           1020
+#define IDC_EDIT_THIRD_SECOND           1021
+#define IDC_SPIN9                       1022
+#define IDC_EDIT_THIRD_ADDED            1023
+#define IDC_SPIN10                      1024
+#define IDC_COMBO_THIRD_SEND            1025
+#define IDC_EDIT_THIRD_SEND_TIME        1026
+#define IDC_SPIN11                      1027
+#define IDC_SPIN6                       1028
+#define IDC_EDIT7                       1029
+#define IDC_EDIT13                      1030
+#define IDC_SPIN12                      1031
+#define IDC_EDIT_SECOND_MINUTE          1032
+#define IDC_COMBO_SECOND_SEND           1033
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_CONTROL_VALUE         1034
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
